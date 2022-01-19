74bd7ad1b4cea2fbfad1fdcc329b99f886465881
adanyear1 <magarciar931@gmail.com>
2022-01-19 01:00:01 -0600

    First commit

---
+++ app/tests.py
@@ -0,0 +1,44 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+"""
+
+import django
+from django.test import TestCase
+
+# TODO: Configure your database in settings.py and sync before running tests.
+
+class ViewTest(TestCase):
+    """Tests for the application views."""
+
+    if django.VERSION[:2] >= (1, 7):
+        # Django 1.7 requires an explicit setup() when running tests in PTVS
+        @classmethod
+        def setUpClass(cls):
+            super(ViewTest, cls).setUpClass()
+            django.setup()
+
+    def test_home(self):
+        """Tests the home page."""
+        response = self.client.get('/')
+        self.assertContains(response, 'Home Page', 1, 200)
+
+    def test_contact(self):
+        """Tests the contact page."""
+        response = self.client.get('/contact')
+        self.assertContains(response, 'Contact', 3, 200)
+
+    def test_about(self):
+        """Tests the about page."""
+        response = self.client.get('/about')
+        self.assertContains(response, 'About', 3, 200)
+
+    def test_services(self):
+        """Tests the services page."""
+        response = self.client.get('/services')
+        self.assertContains(response, 'Services', 3, 200)
+
+    def test_Estimate(self):
+        """Test the quick quote page."""
+        response = self.client.get('/estimate')
+        self.assertContains(response, 'Estimate', 3, 200)
---
+++ sombralandscaping/urls.py
@@ -0,0 +1,18 @@
+"""
+Definition of urls for sombralandscaping.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('estimate/', views.estimate, name='estimate'),
+    path('services', views.services, name='services')
+]
